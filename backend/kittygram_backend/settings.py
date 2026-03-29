18101f0ae305349071707b76b4ac283395306fdc
Ubuntu <ubuntu@fv4u90kmiignqu3glp3a.auto.internal>
2026-03-29 10:30:33 +0000

    Задание выполнено.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,22 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+DEFAULT_DICT = {'SECRET_KEY': 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'}
 
-DEBUG = True
+try:
+    load_dotenv()
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
-ALLOWED_HOSTS = []
+except:
+    SECRET_KEY = DEFAULT_DICT['SECRET_KEY']
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['158.160.234.192', '127.0.0.1', 'localhost', 'kittygram666.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -91,9 +100,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
